839bcc1bea8e26277c70622504c0b676fe71226a
Nikko Dasig <nikkodasig@gmail.com>
2020-09-02 22:08:58 +0800

    Initial commit

---
+++ src/main/java/com/nikkodasig/springbudgetapi/SpringBudgetApiApplication.java
@@ -0,0 +1,13 @@
+package com.nikkodasig.springbudgetapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBudgetApiApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringBudgetApiApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/nikkodasig/springbudgetapi/SpringBudgetApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nikkodasig.springbudgetapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBudgetApiApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
