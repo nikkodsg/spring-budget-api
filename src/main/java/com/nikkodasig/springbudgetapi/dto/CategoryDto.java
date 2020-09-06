4426708beef31daa77550830532a9c69d674c437
Nikko Dasig <nikkodasig@gmail.com>
2020-09-06 12:26:45 +0800

    Add DTOs for Category and Transaction entities

---
+++ src/main/java/com/nikkodasig/springbudgetapi/dto/CategoryDto.java
@@ -0,0 +1,32 @@
+package com.nikkodasig.springbudgetapi.dto;
+
+public class CategoryDto {
+
+  private Long id;
+  private String name;
+  private String type;
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getType() {
+    return type;
+  }
+
+  public void setType(String type) {
+    this.type = type;
+  }
+}
---
+++ src/main/java/com/nikkodasig/springbudgetapi/dto/TransactionDto.java
@@ -0,0 +1,62 @@
+package com.nikkodasig.springbudgetapi.dto;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+public class TransactionDto {
+
+  private Long id;
+
+  @NotNull
+  private Double amount;
+
+  @NotBlank
+  private String description;
+
+  @NotBlank
+  private String date;
+
+  @NotNull
+  private Long categoryId;
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public Double getAmount() {
+    return amount;
+  }
+
+  public void setAmount(Double amount) {
+    this.amount = amount;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public void setDescription(String description) {
+    this.description = description;
+  }
+
+  public String getDate() {
+    return date;
+  }
+
+  public void setDate(String date) {
+    this.date = date;
+  }
+
+  public Long getCategoryId() {
+    return categoryId;
+  }
+
+  public void setCategoryId(Long categoryId) {
+    this.categoryId = categoryId;
+  }
+
+}
