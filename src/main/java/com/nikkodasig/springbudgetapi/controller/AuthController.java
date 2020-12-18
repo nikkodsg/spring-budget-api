a080d2b76fcfb4b7fe656733dcf5768bbf672888
Nikko Dasig <nikkodasig@gmail.com>
2020-12-18 20:33:06 +0800

    Use spring security cors default config

--- src/main/java/com/nikkodasig/springbudgetapi/controller/AuthController.java
+++ src/main/java/com/nikkodasig/springbudgetapi/controller/AuthController.java
@@ -9,13 +9,11 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
--- src/main/java/com/nikkodasig/springbudgetapi/security/WebSecurityConfig.java
+++ src/main/java/com/nikkodasig/springbudgetapi/security/WebSecurityConfig.java
@@ -63,7 +63,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.cors().and().csrf().disable()
+        http.cors().configurationSource(request -> new CorsConfiguration().applyPermitDefaultValues())
+            .and().csrf().disable()
             .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
             .and()
             .authorizeRequests().antMatchers("/api/auth/**").permitAll()
