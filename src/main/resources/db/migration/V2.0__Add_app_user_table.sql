edafa8bf293f61d4460b956894af5c2f19fe9c1a
Nikko Dasig <nikkodasig@gmail.com>
2021-01-19 19:23:10 +0800

    Updated Flyway migration file names

--- src/main/resources/db/migration/V2.0__Add_app_user_table.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE app_user (
-    id bigint GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    email VARCHAR(50) NOT NULL,
-    password VARCHAR(120) NOT NULL,
-    created_at TIMESTAMP NOT NULL,
-    updated_at TIMESTAMP NULL
-);
